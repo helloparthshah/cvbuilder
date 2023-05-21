69fd9f7b14a7f90dd55074383a9b5366e16eec34
Parth Shah <helloparthshah@gmail.com>
2023-05-21 06:47:18 -0700

    Fixes with routing

--- pages/404.js
+++ pages/404.js
@@ -3,7 +3,11 @@ import LoginBar from "@/components/loginBar";
 export default function Custom404() {
     return (
         <>
-            <h1>404 - Page Not Found</h1>
+            <div style={{ width: "100%", display: "flex", justifyContent: "center", alignItems: "center" }}>
+                <h1 style={{ textTransform: "uppercase" }}>
+                    Please login to continue
+                </h1>
+            </div>
         </>
     )
 }
